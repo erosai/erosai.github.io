de401b59ba9a3f54fe5bfe315997981f0147c7b4
erosai <eror.shahini@gmail.com>
2020-12-13 18:28:00 +0100

    changes to sketch.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,13 @@ var canvas;
 var w ;
 
 function windowResized(){
-  resizeCanvas(windowWidth,windowHeight);
+  resizeCanvas(windowWidth/2,windowHeight);
+  canvas.position(windowWidth/4,0,'fixed');
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0,0);
+  canvas = createCanvas(windowWidth/2, windowHeight);
+  canvas.position(windowWidth/4,0,'fixed');
   canvas.style('z-index','-1');
   w1 = new Wave(width/2, height/2, 1, 200, width);
 
@@ -18,8 +19,8 @@ function setup() {
 
 function draw() {
   background(245,245,245,10);
-  w1.barwid = map(mouseX, 0, width, 5, 1);
-  w1.maxhei = map(mouseY, 0, height, height, 1);
+  w1.barwid = map(mouseX*10, 0, width, 5, 1);
+  w1.maxhei = map(mouseY*10, 0, height, height, 1);
   w1.display();
 }
 //Wave Object
@@ -41,11 +42,11 @@ function Wave(x, y, barwid, maxhei, amount)
 	for(this.i=0; this.i<this.amount; this.i++)
     {
       //Time in milliseconds/600 for smoothe transitions
-      this.time = millis()/600;
+      this.time = millis()/10000;
       //Cycling colors depending on time
-      this.r = map(sin(this.time+this.i/90), -1, 1, 0, 255);
-      this.g = map(sin(this.time+22.5+this.i/90), -1, 1, 0, 255);
-      this.b = map(sin(this.time+45+this.i/90), -1, 1, 0, 255);
+      this.r = map(tan(this.time+this.i/90), -1, 1, 0, 255);
+      this.g = map(sin(this.time+22.5+this.i/90), mouseX*-1, 1, 0, 230);
+      this.b = map(sin(this.time+45+this.i/90), mouseX*-1, 1, 0, 230);
       fill(this.r, this.g, this.b);
       //Hight depending on time and i
       this.hei = map(tan(this.i/90 + this.time), -1, 1, 0, this.maxhei);
@@ -59,3 +60,8 @@ function Wave(x, y, barwid, maxhei, amount)
   }
 
 }
+
+
+
+//this.time+this.i/90 for x 
+//this.time+22.5+this.i/90 x
